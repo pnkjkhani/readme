8dd3e4d60fca2bbfb1f90b614ea45f328291b586
Pankaj Khani <pnkjkhani>
2023-09-03 13:37:40 +0530

    modified blogaction

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -35,7 +35,7 @@ async function Blog() {
                       <h1 className="title-font text-lg font-medium text-gray-900 mb-3">{item.title}</h1>
                       <p className="leading-relaxed mb-3">{item.desc ? item.desc : item.body}</p>
                       <div className="flex items-center flex-wrap " >
-                        <Link href={`http://localhost:3000/blog/${item._id}`} className="text-indigo-500  inline-flex items-center md:mb-2 lg:mb-0" style={{ cursor: 'pointer' }}>Learn More
+                        <Link href={`${url}/blog/${item._id}`} className="text-indigo-500  inline-flex items-center md:mb-2 lg:mb-0" style={{ cursor: 'pointer' }}>Learn More
                           <svg className="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2" fill="none" strokeLinecap="round" strokeLinejoin="round">
                             <path d="M5 12h14"></path>
                             <path d="M12 5l7 7-7 7"></path>
